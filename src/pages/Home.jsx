d986ef73daef8d9dbe906468c9e4882ff17c62e0
Julian Muñoz <gomzjulian85@gmail.com>
2024-11-29 01:57:26 -0500

    Agrego boton de comentarios en el menu principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,6 +38,8 @@ function Home() {
   const { user, loginGoogleWithPopUp, logout, observeAuthState } = useAuthStore();
   const [loading, setLoading] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
+  const [comment, setComment] = useState('');
+  const [showCommentBox, setShowCommentBox] = useState(false);
 
   useEffect(() => {
     const checkAuthState = async () => {
@@ -50,10 +52,24 @@ function Home() {
   const handleLogin = useCallback(() => loginGoogleWithPopUp(), [loginGoogleWithPopUp]);
   const handleLogout = useCallback(() => logout(), [logout]);
 
-  if (loading) return <p className="loading-text">Loading...</p>;
-
   const toggleMenu = () => setIsOpen(!isOpen);
 
+  const handleCommentClick = () => {
+    setShowCommentBox(!showCommentBox);
+  };
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleCommentSubmit = () => {
+    alert('Comentario enviado: ' + comment);
+    setComment('');
+    setShowCommentBox(false);
+  };
+  
+
+  if (loading) return <p className="loading-text">Loading...</p>;
 
   return (
     <div>
@@ -62,21 +78,21 @@ function Home() {
           <div className="container-home">
             <Canvas shadows camera={{ position: [0, 5, 10], fov: 50, near: 0.1, far: 1000 }} dpr={[1, 2]} antialias>
               <OrbitControls />
-              <Lights/>
-              <PlayaModel position={[0, -1.5, 0]} scale={[2, 2, 2]} castShadow/>
+              <Lights />
+              <PlayaModel position={[0, -1.5, 0]} scale={[2, 2, 2]} castShadow />
               <Floor />
-              <Html center distanceFactor={20} transform castShadow position={[0,1,0]}>
-                <h1 className="welcome-text" style={{fontFamily: "sans-serif"}}>¡Bienvenido a Diadema Azul!</h1>
+              <Html center distanceFactor={20} transform castShadow position={[0, 1, 0]}>
+                <h1 className="welcome-text" style={{ fontFamily: "sans-serif" }}>¡Bienvenido a Diadema Azul!</h1>
               </Html>
-              <Html center distanceFactor={20} transform castShadow position={[0,-1,0]}>
-                <p style={{color:"white", fontFamily: "sans-serif"}}>Hola, {user.displayName}</p>
+              <Html center distanceFactor={20} transform castShadow position={[0, -1, 0]}>
+                <p style={{ color: "white", fontFamily: "sans-serif" }}>Hola, {user.displayName}</p>
               </Html>
-              <Environment preset="park" background resolution={4096}/>
+              <Environment preset="park" background resolution={4096} />
             </Canvas>
           </div>
-          
+
           <div className="header-content">
-            <img src={logo} alt="Diadema Azul Logo" className="logo" />  
+            <img src={logo} alt="Diadema Azul Logo" className="logo" />
           </div>
           <div className="nav-links">
             <div className="menu-container">
@@ -94,37 +110,57 @@ function Home() {
                   <li><Link to="/population-growth" className="button-option">Crecimiento Demográfico</Link></li>
                   <li><Link to="/pollution-mine" className="button-option">Minería ilegal</Link></li>
                 </ul>
-                
-            
+
                 <button className="styled-logout-button" onClick={handleLogout}>Cerrar sesión</button>
               </div>
             </div>
           </div>
-         
-          <a href="https://github.com/MarvinB97/diadema-azul" target="_blank" class="btn-link">
-  <button class="btn-github">
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M7.99992 1.33331C7.12444 1.33331 6.25753 1.50575 5.4487 1.84078C4.63986 2.17581 3.90493 2.66688 3.28587 3.28593C2.03563 4.53618 1.33325 6.23187 1.33325 7.99998C1.33325 10.9466 3.24659 13.4466 5.89325 14.3333C6.22659 14.3866 6.33325 14.18 6.33325 14C6.33325 13.8466 6.33325 13.4266 6.33325 12.8733C4.48659 13.2733 4.09325 11.98 4.09325 11.98C3.78659 11.2066 3.35325 11 3.35325 11C2.74659 10.5866 3.39992 10.6 3.39992 10.6C4.06659 10.6466 4.41992 11.2866 4.41992 11.2866C4.99992 12.3 5.97992 12 6.35992 11.84C6.41992 11.4066 6.59325 11.1133 6.77992 10.9466C5.29992 10.78 3.74659 10.2066 3.74659 7.66665C3.74659 6.92665 3.99992 6.33331 4.43325 5.85998C4.36659 5.69331 4.13325 4.99998 4.49992 4.09998C4.49992 4.09998 5.05992 3.91998 6.33325 4.77998C6.85992 4.63331 7.43325 4.55998 7.99992 4.55998C8.56659 4.55998 9.13992 4.63331 9.66659 4.77998C10.9399 3.91998 11.4999 4.09998 11.4999 4.09998C11.8666 4.99998 11.6333 5.69331 11.5666 5.85998C11.9999 6.33331 12.2533 6.92665 12.2533 7.66665C12.2533 10.2133 10.6933 10.7733 9.20659 10.94C9.44659 11.1466 9.66659 11.5533 9.66659 12.1733C9.66659 13.0666 9.66659 13.7866 9.66659 14C9.66659 14.18 9.77325 14.3933 10.1133 14.3333C12.7599 13.44 14.6666 10.9466 14.6666 7.99998C14.6666 7.1245 14.4941 6.25759 14.1591 5.44876C13.8241 4.63992 13.333 3.90499 12.714 3.28593C12.0949 2.66688 11.36 2.17581 10.5511 1.84078C9.7423 1.50575 8.8754 1.33331 7.99992 1.33331V1.33331Z"
-        fill="currentcolor"
-      ></path>
-    </svg>
-    <span>View on Github</span>
+            <a href="https://github.com/MarvinB97/diadema-azul" target="_blank" className="btn-link">
+            <button className="btn-github">
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7.99992 1.33331C7.12444 1.33331 6.25753 1.50575 5.4487 1.84078C4.63986 2.17581 3.90493 2.66688 3.28587 3.28593C2.03563 4.53618 1.33325 6.23187 1.33325 7.99998C1.33325 10.9466 3.24659 13.4466 5.89325 14.3333C6.22659 14.3866 6.33325 14.18 6.33325 14C6.33325 13.8466 6.33325 13.4266 6.33325 12.8733C4.48659 13.2733 4.09325 11.98 4.09325 11.98C3.78659 11.2066 3.35325 11 3.35325 11C2.74659 10.5866 3.39992 10.6 3.39992 10.6C4.06659 10.6466 4.41992 11.2866 4.41992 11.2866C4.99992 12.3 5.97992 12 6.35992 11.84C6.41992 11.4066 6.59325 11.1133 6.77992 10.9466C5.29992 10.78 3.74659 10.2066 3.74659 7.66665C3.74659 6.92665 3.99992 6.33331 4.43325 5.85998C4.36659 5.69331 4.13325 4.99998 4.49992 4.09998C4.49992 4.09998 5.05992 3.91998 6.33325 4.77998C6.85992 4.63331 7.43325 4.55998 7.99992 4.55998C8.56659 4.55998 9.13992 4.63331 9.66659 4.77998C10.9399 3.91998 11.4999 4.09998 11.4999 4.09998C11.8666 4.99998 11.6333 5.69331 11.5666 5.85998C11.9999 6.33331 12.2533 6.92665 12.2533 7.66665C12.2533 10.2066 10.6999 10.78 9.21992 10.9466C9.40659 11.1133 9.57992 11.4066 9.63992 11.84C10.0199 12 10.9999 12.3 11.5799 11.2866C11.5799 11.2866 11.9333 10.6466 12.5999 10.6C13.2533 10.6 12.5933 10.5866 12.7466 11C12.7466 11.2066 12.3333 11.2733 11.3333 12.8733C11.3333 13.4266 11.3333 13.8466 11.3333 14C11.4399 14.18 11.3466 14.3866 11.0133 14.3333C13.6666 13.4466 15.5799 10.9466 15.5799 7.99998C15.5799 6.23187 14.8775 4.53618 13.6273 3.28593C13.0082 2.66688 12.2732 2.17581 11.4644 1.84078C10.6555 1.50575 9.79859 1.33331 7.99992 1.33331Z"
+                fill="white"
+              />
+            </svg>
+            Ver el repositorio
   </button>
 </a>
+{/* Botón de comentario */}
+      <button className="bookmarkBtn" onClick={handleCommentClick}>
+        <span className="IconContainer">
+          <svg fill="white" viewBox="0 0 512 512" height="1em" width="16">
+            <path d="M9.42 12.92c-5.37-3.03-8.42-9.67-7.11-16.11 1.02-6.58 5.67-11.92 11.91-13.89 5.73-1.98 12.08-1.3 17.16 2.03 5.07 3.33 7.92 8.88 7.42 14.71-2.3 13.47-14.79 23.34-28.78 24.48-13.73 1.08-27.75-6.68-33.71-18.19L9.42 12.92z"></path>
+          </svg>
+        </span>
+        <p className="text">Comentario</p>
+      </button>
 
-
-         
+      {/* Caja de comentarios */}
+      {showCommentBox && (
+        <div className={`comment-box ${showCommentBox ? 'show' : ''}`}>
+          <textarea
+            placeholder="Escribe tu comentario aquí..."
+            value={comment}
+            onChange={handleCommentChange}
+            rows="4"
+            cols="33"
+            className="comment-textarea"
+          />
+          <button className="comment-submit-button" onClick={handleCommentSubmit}>
+            Enviar
+          </button>
+        </div>
+      )}
+    
   
-
-
-        </>
+</>
       ) : (
         <div className="container-home">
           <Canvas shadows camera={{ position: [0, 1, 5], fov: 50 }}>
@@ -135,7 +171,7 @@ function Home() {
             <Html center distanceFactor={4} transform position={[0,0.5,0]} castShadow>
                 <h1 className="welcome-text">¡Bienvenido a Diadema Azul!</h1>
             </Html>
-            <Html center distanceFactor={4} transform position={[0,-5,0]} castShadow>
+            <Html center distanceFactor={4} transform position={[0,0,0]} castShadow>
               <p style={{color:"white"}}>"Exploramos la importancia del agua para la vida, su conservación y el impacto ambiental de su uso."</p>
             </Html>
             <Environment preset="night" background resolution={4096} />
@@ -150,19 +186,5 @@ function Home() {
   );
 }
 
+
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
